2b4f8903d1d32cc022b75042b55fd8b9cbddef44
Vitória Rosa <vmainardirosa@gmail.com>
2018-06-20 21:42:28 -0300

    listener update and connections created only on demand

--- src/ServerNetwork.cpp
+++ src/ServerNetwork.cpp
@@ -11,37 +11,23 @@ ServerNetwork::ServerNetwork(Connection *connection) {
 
   void ServerNetwork::run() {
     list <string> otherServers = read("servers.txt");
+    // the first ip is mine so I remove from the list
     string myIP = otherServers[0];
-    bool skip_first = true;
-
-    foreach (otherServers, string server) {
-      if skip_first { // because first IP is my IP
-        skip_first = false;
-        continue;
-      }
-
-      Connection* connection = new Connection();
-      if (connection->connect(server)) {
-        onlineServers[server] = connection;
-      } else {
-        delete connection;
-      }
-    }
+    otherServers.pop_front();
 
     mainloop();
   }
 
   void ServerNetwork::mainloop() {
-    // run just generated the list of connections, I should call elections
+    // run just generated the list of servers, I should call elections
     election_coordinator();
-    while(true) {
-      // keep receiving messages from all to check if one of them recovered and wants an election
-      foreach (onlineServers, string server) {
-        Message msg = server->receive(expected_types); //precisa de um timeout aqui pra dar continue ou ele faz sozinho?
 
-        if (msg.type == Message::T_ELECTION) {
-          election_coordinator();
-        }
+    Socket listener(1996);
+    listener.bind_server();
+    while(true) {
+      Message msg = Message::parse(listener.receive());
+      if (msg.type == Message::T_ELECTION) {
+        election_coordinator();
       }
     }
   }
@@ -50,33 +36,34 @@ ServerNetwork::ServerNetwork(Connection *connection) {
     bool i_am_bully = true;
     string the_bully = "";
 
-    foreach (onlineServers, string candidate) {
+    foreach (otherServers, string candidate) {
       bool i_am_smaller = false;
 
-      i_am_smaller = (myIP < candidate.username);
+      i_am_smaller = (myIP < candidate);
       
       if i_am_smaller {
         // send ELECTION message to the candidate whose IP is bigger than mine
-        candidate->send(Message::T_ELECTION);
-        candidate->receive_ack();
+        bully_connection = connection->connect(candidate)
+        bully_connection->send(Message::T_ELECTION);
+        bully_connection->receive_ack();
 
         // if I receive an ALIVE message I'm not the bully
-        Message msg = candidate->receive(expected_types);
+        Message msg = bully_connection->receive(expected_types);
         if (msg.type == Message::T_ALIVE) {
           i_am_bully = false;
           // if the IP is the biggest I have seen then he is the bully
-          if (candidate.username > the_bully)
-          the_bully = candidate;
+          if (bully_connection.username > the_bully)
+          the_bully = bully_connection;
         }
-        // ***** TODO: pensar no caso de não receber o ALIVE = tirar candidato da lista, pode ser só timeout ou problema temporário *****
       }
     }
 
     if i_am_bully {
       // send COORD message to other servers saying that I'm the coordinator
-      foreach (onlineServers, string loser) {
-        looser->send(Message::T_COORD);
-        looser->receive_ack();
+      foreach (otherServers, string smaller) {
+        loser = connection->connect(smaller)
+        loser->send(Message::T_COORD);
+        loser->receive_ack();
       }
     } else {
       Message msg = the_bully->receive(expected_types);
